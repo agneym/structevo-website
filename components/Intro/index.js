dbaddfe22078d1201d273c2cb1f5574f8da49fdd
Agney <agney.menon@bigbinary.com>
2020-05-18 01:05:14 +0530

    build out sections

--- components/Intro/index.js
+++ components/Intro/index.js
@@ -2,17 +2,23 @@ import React from "react";
 import styled from "styled-components";
 
 import FirstSection from "./FirstSection";
+import SecondSection from "./SecondSection";
 
 const Section = styled.section`
   max-width: 1280px;
-  margin: 0 auto;
+  margin: 5rem auto;
   padding: 0 1rem;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  grid-gap: 5rem 0;
 `;
 
 function Intro() {
   return (
     <Section>
       <FirstSection />
+      <SecondSection />
     </Section>
   );
 }
